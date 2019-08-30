e0c9bd240646903bf79173b32a7ad6e201c82aa0
Jhonata Bruno Fucks <jhonata.b.f@hotmail.com>
2019-08-29 22:24:53 -0300

    Atualização

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -1,18 +1,26 @@
 package loteria;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Loteria {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int[] valores = new int[] {0, 0, 0, 0, 0, 0};
+        //int[] valores2 = new int[6];
         
+        imprimeVetor(valores);
+
         System.out.println("Digite 6 números");
         for (int i = 0; i < 6; i++) {
             int num = -1;
             while (num == -1)
                 num = capNum(in, i);
+            valores[i] = num; 
         }
+        
+        imprimeVetor(valores);
         
     }
     
@@ -26,6 +34,13 @@ public class Loteria {
         else
             return -1;
        
+    }
+    
+    public static void imprimeVetor(int[] vet){
+        for (int i = 0; i < vet.length; i++) {
+            System.out.print(vet[i] + " ");
+        }
+        System.out.println();
     }
     
 }
