da888a942b7130b07adfe3f74afff1b4ed3ab6d2
Jhonata Bruno Fucks <jhonata.b.f@hotmail.com>
2019-08-29 21:51:39 -0300

    Atualização

--- Loteria/src/loteria/Loteria.java
+++ Loteria/src/loteria/Loteria.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public class Loteria {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         
